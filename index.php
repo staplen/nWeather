ef81c949c9d9a22b7c7f35d6be7c6e9bb28805d6
Nico Staple <nico.staple@gmail.com>
2014-04-17 16:44:14 -0400

    status bar style work

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <link href="img/favicon.ico?v3" rel="shortcut icon">
     <meta content="yes" name="apple-mobile-web-app-capable">
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="white-translucent">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-mobile-web-app-title" content="nWeather">
 
     <!-- iPhone -->
